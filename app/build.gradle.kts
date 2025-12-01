c621995e498bf380d18f648e4c9d6e9087e5397b
samea-jesus0 <sameasilva198@gmail.com>
2025-12-01 18:59:57 -0300

    Mudança das cores dos textos dos itens

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,8 +56,8 @@ dependencies {
     implementation(libs.androidx.room.ktx)
 
     // usar o cep
-    implementation(libs.retrofit)
-    implementation(libs.retrofit2.converter.gson)
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 
 
     testImplementation(libs.junit)
